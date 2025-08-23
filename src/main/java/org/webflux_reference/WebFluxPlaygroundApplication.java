3d69468e2d5539251d8a0f26b7671bd941f1f5cf
marwhals <marjan.mubarok2012@gmail.com>
2025-08-23 21:12:54 +0100

    Add integration tests

--- src/main/java/org/webflux_reference/WebFluxPlaygroundApplication.java
+++ src/main/java/org/webflux_reference/WebFluxPlaygroundApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+/**
+ * Change annotations for respective package
+ */
 @SpringBootApplication(scanBasePackages = "org.webflux_reference.reactive_crud_api")
 @EnableR2dbcRepositories(basePackages = "org.webflux_reference.reactive_crud_api.repository")
 public class WebFluxPlaygroundApplication {
