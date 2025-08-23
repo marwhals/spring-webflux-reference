5559455a9b67b64d046d4eb5bfc03a05e30847f7
marwhals <marjan.mubarok2012@gmail.com>
2025-08-23 19:20:26 +0100

    Add controller

--- src/main/java/org/webflux_reference/reactive_crud_api/repository/CustomerRepository.java
+++ src/main/java/org/webflux_reference/reactive_crud_api/repository/CustomerRepository.java
@@ -1,15 +1,22 @@
 package org.webflux_reference.reactive_crud_api.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import org.webflux_reference.spring_r2dbc.entity.Customer;
+
+import org.webflux_reference.reactive_crud_api.entity.Customer;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
 
-    Flux<Customer> findByName(String name);
+    @Modifying
+    @Query("delete from customer where id=:id")
+    Mono<Boolean> deleteCustomerById(Integer id);
 
-    Flux<Customer> findByEmailEndingWith(String email);
+    Flux<Customer> findBy(Pageable pageable);
 
 }
