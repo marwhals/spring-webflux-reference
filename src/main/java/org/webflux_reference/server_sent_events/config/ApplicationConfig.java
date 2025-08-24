17a9ad57c0f1888215697dad2283bdd28817ecd7
marwhals <marjan.mubarok2012@gmail.com>
2025-08-24 15:53:53 +0100

    Setup sink

---
+++ src/main/java/org/webflux_reference/server_sent_events/config/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package org.webflux_reference.server_sent_events.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.webflux_reference.streaming.dto.ProductDto;
+import reactor.core.publisher.Sinks;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public Sinks.Many<ProductDto> sink(){
+        return Sinks.many().replay().limit(1);
+    }
+
+}
