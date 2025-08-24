c1eb857a1a3764d2da958f11eeb5ce0b2d2c2927
marwhals <marjan.mubarok2012@gmail.com>
2025-08-24 14:39:32 +0100

    Nested Routers

--- src/main/java/org/webflux_reference/functional_endpoints/config/RouterConfiguration.java
+++ src/main/java/org/webflux_reference/functional_endpoints/config/RouterConfiguration.java
@@ -21,14 +21,20 @@ public class RouterConfiguration {
     @Bean
     public RouterFunction<ServerResponse> customerRoutes() {
         return RouterFunctions.route()
-                .GET("/customers", this.customerRequestHandler::allCustomers)
-                .GET("/customers/paginated", this.customerRequestHandler::paginatedCustomers)
-                .GET("/customers/{id}", this.customerRequestHandler::getCustomer)
+                .path("customers", this::customerRoutes2)
                 .POST("/customers", this.customerRequestHandler::saveCustomer)
                 .PUT("/customers/{id}", this.customerRequestHandler::updateCustomer)
                 .DELETE("/customers/{id}", this.customerRequestHandler::deleteCustomer)
                 .onError(CustomerNotFoundException.class, this.exceptionHandler::handleException)
                 .onError(InvalidInputException.class, this.exceptionHandler::handleException)
+                .build();
+    }
+
+    private RouterFunction<ServerResponse> customerRoutes2() {
+        return RouterFunctions.route()
+                .GET( "/paginated",this.customerRequestHandler::paginatedCustomers)
+                .GET( "/{id}", this.customerRequestHandler::getCustomer)
+                .GET( this.customerRequestHandler::allCustomers)
                 .build();
     }
 
