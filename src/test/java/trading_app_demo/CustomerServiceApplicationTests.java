5db5715be2832110f70aace6e5b39220b992a264
marwhals <marjan.mubarok2012@gmail.com>
2025-10-28 12:52:51 +0000

    Setup trading app demo project

---
+++ src/test/java/trading_app_demo/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package trading_app_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
