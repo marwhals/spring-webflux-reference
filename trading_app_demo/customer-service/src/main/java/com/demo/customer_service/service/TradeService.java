ae78616b320deed85250a661d544d15511bf19c2
marwhals <marjan.mubarok2012@gmail.com>
2025-10-28 17:22:42 +0000

    Add sell reqeuest handler

--- trading_app_demo/customer-service/src/main/java/com/demo/customer_service/service/TradeService.java
+++ trading_app_demo/customer-service/src/main/java/com/demo/customer_service/service/TradeService.java
@@ -28,7 +28,7 @@ public class TradeService {
     public Mono<StockTradeResponse> trade(Integer customerId, StockTradeRequest request) {
         return switch (request.action()) {
             case BUY -> this.buyStock(customerId, request);
-            case SELL -> throw new RuntimeException("Not Implemented"); //this.sellStock(customerId, request);
+            case SELL -> this.sellStock(customerId, request);
         };
     }
 
@@ -41,8 +41,6 @@ public class TradeService {
         var portfolioItemMono = this.portfolioItemRepository.findByCustomerIdAndTicker(customerId, request.ticker())
                 .defaultIfEmpty(EntityDtoMapper.toPortfolioItem(customerId, request.ticker()));
 
-
-
         return customerMono.zipWhen(customer -> portfolioItemMono)
                 .flatMap(t -> this.executeBuy(t.getT1(), t.getT2(), request));
     }
@@ -50,12 +48,31 @@ public class TradeService {
     private Mono<StockTradeResponse> executeBuy(Customer customer, PortfolioItem portfolioItem, StockTradeRequest request) {
         customer.setBalance(customer.getBalance() - request.totalPrice());
         portfolioItem.setQuantity(portfolioItem.getQuantity() + request.quantity());
+        return saveAndBuildResponse(customer, portfolioItem, request);
+    }
+
+    private Mono<StockTradeResponse> sellStock(Integer customerId, StockTradeRequest request) {
+        var customerMono = this.customerRepository.findById(customerId)
+                .switchIfEmpty(ApplicationExceptions.customerNotFound(customerId));
+
+        var portfolioItemMono = this.portfolioItemRepository.findByCustomerIdAndTicker(customerId, request.ticker())
+                .filter(pi -> pi.getQuantity() >= request.quantity())
+                .switchIfEmpty(ApplicationExceptions.insufficientShares(customerId));
+
+        return customerMono.zipWhen(customer -> portfolioItemMono)
+                .flatMap(t -> this.executeSell(t.getT1(), t.getT2(), request));
+    }
+
+    private Mono<StockTradeResponse> executeSell(Customer customer, PortfolioItem portfolioItem, StockTradeRequest request) {
+        customer.setBalance(customer.getBalance() + request.totalPrice());
+        portfolioItem.setQuantity(portfolioItem.getQuantity() - request.quantity());
+        return this.saveAndBuildResponse(customer, portfolioItem, request);
+    }
+
+    private Mono<StockTradeResponse> saveAndBuildResponse(Customer customer, PortfolioItem portfolioItem, StockTradeRequest request) {
         var response = EntityDtoMapper.toStockTradeResponse(request, customer.getId(), customer.getBalance());
         return Mono.zip(this.customerRepository.save(customer), this.portfolioItemRepository.save(portfolioItem))
                 .thenReturn(response);
     }
-
-//    private Mono<StockTradeResponse> sellStock(Integer customerId, StockTradeRequest request) {
-//    }
 
 }
