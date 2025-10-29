0ba4eda395a54c628a5ee73e1240edcb7c6201ae
marwhals <marjan.mubarok2012@gmail.com>
2025-10-29 15:05:05 +0000

    Hot price stream

--- trading_app_demo/aggregator-service/src/main/java/com/aggregator/client/StockServiceClient.java
+++ trading_app_demo/aggregator-service/src/main/java/com/aggregator/client/StockServiceClient.java
@@ -9,13 +9,17 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class StockServiceClient {
 
     private static final Logger log = LoggerFactory.getLogger(StockServiceClient.class);
     private final WebClient client;
+    private Flux<PriceUpdate> flux;
 
     public StockServiceClient(WebClient client) {
         this.client = client;
@@ -28,12 +32,26 @@ public class StockServiceClient {
                 .bodyToMono(StockPriceResponse.class);
     }
 
+    public Flux<PriceUpdate> priceUpdateFlux() {
+        if(Objects.isNull(this.flux)) {
+            this.flux = this.getPriceUpdates();
+        }
+        return this.flux;
+    }
+
     public Flux<PriceUpdate> getPriceUpdates() {
         return this.client.get()
                 .uri("/stock/price-stream")
                 .accept(MediaType.APPLICATION_NDJSON)
                 .retrieve()
-                .bodyToFlux(PriceUpdate.class);
+                .bodyToFlux(PriceUpdate.class)
+                .retryWhen(retry())
+                .cache(1);
+    }
+
+    private Retry retry() {
+        return Retry.fixedDelay(100, Duration.ofSeconds(1))
+                .doBeforeRetry(rs -> log.error("stock service price stream call failed. retrying: {}", rs.failure().getMessage()));
     }
 
 }
