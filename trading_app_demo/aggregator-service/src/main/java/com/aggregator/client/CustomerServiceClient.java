05da5fd3141b2e68e1ecf559c069e724d3b83663
marwhals <marjan.mubarok2012@gmail.com>
2025-10-29 15:34:24 +0000

    Customer service client

---
+++ trading_app_demo/aggregator-service/src/main/java/com/aggregator/client/CustomerServiceClient.java
@@ -0,0 +1,53 @@
+package com.aggregator.client;
+
+import com.aggregator.dto.CustomerInformation;
+import com.aggregator.dto.StockTradeRequest;
+import com.aggregator.dto.StockTradeResponse;
+import com.aggregator.exceptions.ApplicationExceptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException.NotFound;
+import org.springframework.web.reactive.function.client.WebClientResponseException.BadRequest;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+public class CustomerServiceClient {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomerServiceClient.class);
+    private final WebClient client;
+
+    public CustomerServiceClient(WebClient client) {
+        this.client = client;
+    }
+
+    public Mono<CustomerInformation> getCustomerInformation(Integer customerId) {
+        return this.client.get()
+                .uri("/customers/{customerId}/trade", customerId)
+                .retrieve()
+                .bodyToMono(CustomerInformation.class)
+                .onErrorResume(NotFound.class,
+                        ex -> ApplicationExceptions.customerNotFound(customerId)
+                );
+    }
+
+    public Mono<StockTradeResponse> trade(Integer customerId, StockTradeRequest request) {
+        return this.client.post()
+                .uri("/customer/{customerId}/trade", customerId)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(StockTradeResponse.class)
+                .onErrorResume(NotFound.class,ex -> ApplicationExceptions.customerNotFound(customerId))
+                .onErrorResume(BadRequest.class, this::handleException);
+    }
+
+    private <T> Mono<T> handleException(BadRequest exception) {
+        var pd = exception.getResponseBodyAs(ProblemDetail.class);
+        var message = Objects.nonNull(pd) ? pd.getDetail() : exception.getMessage();
+        log.error("customer service problem detail: {}", pd);
+        return ApplicationExceptions.invalidTradeRequest(message);
+    }
+
+}
